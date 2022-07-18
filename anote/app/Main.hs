e5b280ffcff784b7a13fc159d5de5300d0ef8008
Alexander Foremny <aforemny@posteo.de>
2022-07-18 09:22:22 +0200

    anote: add `create` cmd

--- anote/app/Main.hs
+++ anote/app/Main.hs
@@ -13,22 +13,25 @@ import System.Exit
 import System.FilePath
 
 data Args
-  = EditArgs {includes :: [FilePath], filePath :: FilePath}
+  = CreateArgs {includes :: [FilePath], filePath :: FilePath}
+  | EditArgs {includes :: [FilePath], filePath :: FilePath}
   | ListArgs {includes :: [FilePath]}
   | ShowArgs {includes :: [FilePath], filePath :: FilePath}
 
-editArgs, listArgs, showArgs :: Parser Args
+createArgs, editArgs, listArgs, showArgs :: Parser Args
+createArgs = CreateArgs <$> includesArg <*> fileArg
 editArgs = EditArgs <$> includesArg <*> fileArg
 listArgs = ListArgs <$> includesArg
 showArgs = ShowArgs <$> includesArg <*> fileArg
 
-editCmd, listCmd, showCmd :: Mod CommandFields Args
+createCmd, editCmd, listCmd, showCmd :: Mod CommandFields Args
+createCmd = command "create" (info createArgs fullDesc)
 editCmd = command "edit" (info editArgs fullDesc)
 listCmd = command "list" (info listArgs fullDesc)
 showCmd = command "show" (info showArgs fullDesc)
 
 mainArgs :: ParserInfo Args
-mainArgs = info (hsubparser (editCmd <> listCmd <> showCmd) <**> helper) idm
+mainArgs = info (hsubparser (createCmd <> editCmd <> listCmd <> showCmd) <**> helper) idm
 
 includesArg :: Parser [FilePath]
 includesArg = many (strOption (long "include" <> short 'I' <> metavar "DIR"))
@@ -46,6 +49,9 @@ main :: IO ()
 main = do
   includes' <- maybe [] (splitOn ":") <$> lookupEnv "ATOOLS_PATH"
   execParser mainArgs >>= \case
+    CreateArgs {..} -> do
+      h <- S.open (includes' ++ includes)
+      S.editFile h (S.fromString filePath)
     EditArgs {..} -> do
       h <- S.open (includes' ++ includes)
       S.editFile h (S.fromString filePath)
